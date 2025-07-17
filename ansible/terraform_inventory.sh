7dfe216ee767bdbe5a65a20b6bb53513bf142a0e
Chris <151184951+Chrisfisch01@users.noreply.github.com>
2025-07-17 20:30:34 +0200

    HDFS Datanodes implemented
    Speichern von Daten vorbereitet

--- ansible/terraform_inventory.sh
+++ ansible/terraform_inventory.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 # terraform_inventory.sh - Dynamisches Ansible Inventory aus Terraform-Output (Bash-Version)
 
 
@@ -9,14 +9,34 @@ ALL_IPS=($(echo "$TF_OUTPUT" | jq -r '.all_private_ips.value[]'))
 MASTER_IP=${ALL_IPS[0]}
 WORKER_IPS=("${ALL_IPS[@]:1}")
 
+# Hostnamen generieren (ohne assoziative Arrays)
+MASTER_HOSTNAME="mjcs2-k8s-master"
+WORKER_HOSTNAMES=""
+for i in $(seq 0 $((${#WORKER_IPS[@]} - 1))); do
+	WORKER_HOSTNAMES="$WORKER_HOSTNAMES mjcs2-k8s-worker-$i"
+done
+
 # JSON-Ausgabe erzeugen
 printf '{\n'
-printf '  "master": { "hosts": ["%s"] },\n' "$MASTER_IP"
+printf '  "master": { "hosts": ["%s"] },\n' "$MASTER_HOSTNAME"
 printf '  "worker": { "hosts": ['
-for ip in "${WORKER_IPS[@]}"; do printf '"%s",' "$ip"; done | sed 's/,$//'
+for i in $(seq 0 $((${#WORKER_IPS[@]} - 1))); do
+	hn="mjcs2-k8s-worker-$i"
+	printf '"%s",' "$hn"
+done | sed 's/,$//'
 printf '] },\n'
 printf '  "all": { "children": ["master", "worker"] },\n'
 printf '  "_meta": {\n    "hostvars": {'
-for ip in "${ALL_IPS[@]}"; do printf '"%s": {"ansible_user": "ubuntu", "ansible_ssh_private_key_file": "~/.ssh/id_rsa"},' "$ip"; done | sed 's/,$//'
+
+# Master hostvars
+printf '"%s": {"ansible_host": "%s", "ansible_user": "ubuntu", "ansible_ssh_private_key_file": "~/.ssh/id_rsa"},' "$MASTER_HOSTNAME" "$MASTER_IP"
+
+# Worker hostvars
+for i in $(seq 0 $((${#WORKER_IPS[@]} - 1))); do
+	hn="mjcs2-k8s-worker-$i"
+	ip="${WORKER_IPS[$i]}"
+	printf '"%s": {"ansible_host": "%s", "ansible_user": "ubuntu", "ansible_ssh_private_key_file": "~/.ssh/id_rsa"},' "$hn" "$ip"
+done | sed 's/,$//'
+
 printf '}\n  }\n'
 printf '}\n'
