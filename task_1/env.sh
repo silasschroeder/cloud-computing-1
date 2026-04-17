e345e34f925d6de98e838a96a0ea9b02fc213ee9
Silas <132845575+silasschroeder@users.noreply.github.com>
2026-04-17 13:28:25 +0200

    Delete task_1/env.sh
    upsi

--- task_1/env.sh
+++
@@ -1,6 +0,0 @@
-# DHBW OpenStack Login Credentials used by initialisation.tf
-export OS_AUTH_URL="https://stack.dhbw.cloud:5000"
-export OS_USERNAME="pfisterer-cloud-lecture"
-export OS_PASSWORD="ss2025"
-export OS_PROJECT_ID="6c1ae45e04f24dc695d6f526fce253c6"
-export OS_USER_DOMAIN_NAME="default"
